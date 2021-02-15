3e1038fdbde0b1749ceeebf169ffb9ab2c89cc20
KhantSu <khantsuyinwin2001@gmail.com>
2021-02-15 10:41:01 +0630

    Added new files

---
+++ js/been_together.js
@@ -0,0 +1,22 @@
+const myDate = '2018-9-9';
+
+//count days
+const oldDate = new Date(myDate);
+const newDate = new Date();
+
+const dateDiff = Math.abs(newDate.getTime() - oldDate.getTime());
+const days = Math.ceil(dateDiff / (1000 * 3600 * 24));
+
+
+document.querySelector(".days").innerHTML = days + ' Days';
+
+
+//count seconds
+
+setInterval(()=>{
+  const sOldDate = new Date(myDate);
+  const sNewDate = new Date();
+  const secondDiff = (sNewDate.getTime() - sOldDate.getTime())/1000;
+  const seconds = Math.ceil(secondDiff);
+  document.querySelector(".seconds").innerHTML = seconds + ' Seconds';
+},1000);
