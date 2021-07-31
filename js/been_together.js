c19dc355c515f51ebea236da0f32b3450797a884
mtk246 <minthukyaw454@gmail.com>
2021-07-31 20:31:17 +0630

    Added bootstrap, fontawesome, jQuery and make some changes for responsiveness

--- js/been_together.js
+++ js/been_together.js
@@ -1,4 +1,4 @@
-const myDate = '2018-9-9';
+const myDate = "2018-9-9";
 
 //count days
 const oldDate = new Date(myDate);
@@ -7,16 +7,14 @@ const newDate = new Date();
 const dateDiff = Math.abs(newDate.getTime() - oldDate.getTime());
 const days = Math.ceil(dateDiff / (1000 * 3600 * 24));
 
-
-document.querySelector(".days").innerHTML = days + ' Days';
-
+$(".days").html(days + " Days");
 
 //count seconds
 
-setInterval(()=>{
+setInterval(() => {
   const sOldDate = new Date(myDate);
   const sNewDate = new Date();
-  const secondDiff = (sNewDate.getTime() - sOldDate.getTime())/1000;
+  const secondDiff = (sNewDate.getTime() - sOldDate.getTime()) / 1000;
   const seconds = Math.ceil(secondDiff);
-  document.querySelector(".seconds").innerHTML = seconds + ' Seconds';
-},1000);
+  document.querySelector(".seconds").innerHTML = seconds + " Seconds";
+}, 1000);
